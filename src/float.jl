0cb6dbbf5c6ffa356107c6e34b11bd320985505b
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-04-10 23:55:40 -0400

    Changed shiftwidth to 2

--- src/float.jl
+++ src/float.jl
@@ -1,26 +1,26 @@
 
 function chopzero!{T<:Real,D}(arr ::Array{Complex{T},D};
                               rtol::Real=sqrt(eps), atol::Real=0)
-    for i in eachindex(arr)
-        if isapprox(real(arr[i]), 0, rtol, atol)
-            arr[i] = 1im * imag(arr[i])
-        end
-        if isapprox(imag(arr[i]), 0, rtol, atol)
-            arr[i] = real(arr[i])
-        end
+  for i in eachindex(arr)
+    if isapprox(real(arr[i]), 0, rtol, atol)
+      arr[i] = 1im * imag(arr[i])
     end
-    arr
+    if isapprox(imag(arr[i]), 0, rtol, atol)
+      arr[i] = real(arr[i])
+    end
+  end
+  arr
 end
 
 
 function chopzero!{T<:Real,D}(arr ::Array{T, D};
                               rtol::Real=sqrt(eps), atol::Real=0)
-    for i in eachindex(arr)
-        if isapprox(arr[i], 0, rtol, atol)
-            arr[i] = 0
-        end
+  for i in eachindex(arr)
+    if isapprox(arr[i], 0, rtol, atol)
+      arr[i] = 0
     end
-    arr
+  end
+  arr
 end
 
 
@@ -41,11 +41,11 @@ end
 
 function chopzero{T<:Real,D}(arr_in :: Array{T, D};
                              rtol::Real=sqrt(eps), atol::Real=0)
-    arr = copy(arr_in)
-    for i in eachindex(arr)
-        if isapprox(arr[i], 0, rtol, atol)
-            arr[i] = 0
-        end
+  arr = copy(arr_in)
+  for i in eachindex(arr)
+    if isapprox(arr[i], 0, rtol, atol)
+      arr[i] = 0
     end
-    arr
+  end
+  arr
 end
--- src/function.jl
+++ src/function.jl
@@ -1,19 +1,19 @@
 function funcplus(func1, funcs...)
-    function (k...)
-        v = func1(k...)
-        for func in funcs
-            v += func(k...)
-        end
-        v
+  function (k...)
+    v = func1(k...)
+    for func in funcs
+      v += func(k...)
     end
+    v
+  end
 end
 
 function functimes(func1, funcs...)
-    function (k...)
-        v = func1(k...)
-        for func in funcs
-            v *= func(k...)
-        end
-        v
+  function (k...)
+    v = func1(k...)
+    for func in funcs
+      v *= func(k...)
     end
+    v
+  end
 end
